d47c3f47d76fa0df50f3800e84ade8ab8d2a707d
Eric <elum1234@gmail.com>
2017-04-06 18:17:33 -0400

    grid layout

---
+++ script.js
@@ -0,0 +1,11 @@
+
+
+$(document).ready(function () {
+  for (var i=0; i<16;i++) {
+    for (var y=0; y<16; y++) {
+    var $square = "<div class='square'> </div>"
+    $('#canvas').append($square);
+  }
+
+}
+});
